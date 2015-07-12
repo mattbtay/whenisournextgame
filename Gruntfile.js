ff3b8c70bfdd3ccd2441cac4b43223ae21ce2d88
Matt Taylor <mattbtay@gmail.com>
2015-07-11 22:33:07 -0500

    add cname and update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -325,6 +325,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
+            'CNAME',
             'styles/fonts/{,*/}*.*'
           ]
         }, {
